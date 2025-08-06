f11665fbb8186ce70c02d0763c88a65617a1afc3
netsharkinformatica <helio.tome2024@gmail.com>
2025-08-06 10:16:49 -0300

    criado o dataframe streamlit

--- visualizando_tb.py
+++ visualizando_tb.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-caminho_compras = os.path.abspath("/datasets/compras.csv")
+caminho_compras = os.path.abspath("datasets/compras.csv")
 #caminho_compras = "analisedadosstreamlit/datasets/compras.csv"  # Adicione o nome do arquivo
 df_compras = pd.read_csv(caminho_compras, sep=";", decimal=",")
 df_compras= pd.read_csv(caminho_compras, sep=";", decimal=",")
 
-print(df_compras) 
+st.dataframe(df_compras) 
 
